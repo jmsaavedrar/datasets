54fd2359ca0f2bb9dbc36252885948f3d0282977
jose.saavedra <jose.saavedra>
2023-06-01 17:46:12 -0400

    qd

--- test2.py
+++ test2.py
@@ -4,6 +4,9 @@ import tfds_mnist.tfds_mnist as tfds_mnist
 if __name__ == '__main__' :
     dir ='~/tensorflow_datasets/'
     ds = tfds.load('tfds_mnist',data_dir = dir)
-    ds_train = ds['train']
-    for sample in ds_train :
-        print(sample['image'])    
+    ds_test = ds['test'].shuffle(1024).batch(10)
+        
+    ds_test = ds_test.take(1)
+    for sample in ds_test:
+        print(sample['image'].numpy())
+        
